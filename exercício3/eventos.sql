29da313e411a05e95a367b5b54d5c062fd347cde
Victor-Lutz <83316369+Victor-Lutz@users.noreply.github.com>
2021-10-15 22:41:46 -0300

    Delete exercício3 directory

--- exercício3/eventos.sql
+++
@@ -1,7 +0,0 @@
-create table `exercício3`.`eventos` (
-  `id` int not null auto_increment,
-  `nome` varchar(45) null,
-  `data` date null,
-  `hora` time null,
-  `tipo` varchar(255) NULL,
-  primary key (`id`));
