eea39e556f94d45ccda6e1999d1cdadfb4000720
Victor-Lutz <83316369+Victor-Lutz@users.noreply.github.com>
2021-10-15 22:42:42 -0300

    Delete exercício6 directory

--- exercício6/engenheiros.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `exec6`.`engenheiros` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
-  `cpf` VARCHAR(45) NOT NULL,
-  `crea` VARCHAR(45) NOT NULL,
-  `telefone` VARCHAR(45) NOT NULL,
-  `obra` INT NOT NULL,
-  PRIMARY KEY (`id`));
--- exercício6/operarios.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `exec6`.`operarios` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `CTPS` VARCHAR(8) NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `endereço` VARCHAR(45) NOT NULL,
-  `telefones` VARCHAR(45) NOT NULL,
-  `obra` INT NOT NULL,
-  PRIMARY KEY (`id`));
